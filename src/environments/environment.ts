51f315664d4dfa33bc8a3df6590df96720f4f656
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2025-08-06 14:20:52 +0200

    fix : api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://185.97.144.63:7113',
-  apiUrl: 'https://api.just-add.click:7113',
+  apiUrl: 'https://api.just-add.click',
 };
