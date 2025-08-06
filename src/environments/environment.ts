07d2d675e2d0d73710a9e52f3800ae797028a888
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2025-08-06 14:18:04 +0200

    prod url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://185.97.144.63:7113',
+  // apiUrl: 'http://185.97.144.63:7113',
+  apiUrl: 'https://api.just-add.click:7113',
 };
