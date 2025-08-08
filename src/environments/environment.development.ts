b44c3339dea84c89587c845a5c55428821f7c4d1
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2025-08-08 17:29:44 +0200

    right url api

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7113',
+  // apiUrl: 'https://localhost:7113',
   // apiUrl: 'http://185.97.144.63:7113',
-  // apiUrl: 'https://api.just-add.click',
+  apiUrl: 'https://api.just-add.click',
 };
